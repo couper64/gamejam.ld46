34c991b9f48410bf8670446296b1ccb103cc4b63
Vladislav Li <couper64@gmail.com>
2020-04-18 13:33:22 +0100

    Added unity projects folder with a first project in it.

---
+++ unity_projects/project_1_abduct_baby/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿namespace LD46
+{
+    using UnityEngine;
+
+    /// <summary>
+    /// One script to rule them all.
+    /// </summary>
+    public class GameManager : MonoBehaviour
+    {
+
+
+        private void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
+        // Days.
+        // Iterate of days.
+        // End of the game.
+    }
+}
